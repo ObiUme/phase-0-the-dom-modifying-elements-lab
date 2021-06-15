c226d70299f9d81371ec7caa4ed3bf298c09738d
Obi <mr.obiume@gmail.com>
2021-06-14 20:21:52 -0700

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code here!
+document.querySelector('main').remove(0);
+const newHeader = document.createElement('h1');
+newHeader.id= "victory";
+newHeader.innerHTML = "<p><strong>Obi</strong> is the champion</p>"
+
+
